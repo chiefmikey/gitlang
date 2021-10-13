687d205e0f304d539a19887221a10431a4877003
chiefmikey <wolfemikl@gmail.com>
2021-10-13 11:45:31 -0600

    Add get request to index

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
 import Router from '@koa/router';
-import getRouter from './requests/get.js';
+import getLanguages from './requests/languages.js';
+import getRepos from './requests/repos.js';
+import allNames from './helpers/names.js';
+import getSize from './helpers/size.js';
 
-const router = new Router({ prefix: '/' });
+const router = new Router();
 
-router.use(getRouter.routes(), getRouter.allowedMethods());
+router.get('/languages', async (ctx) => {
+  try {
+    const { owner } = ctx.request.query;
+    const repos = await getRepos(owner);
+    const names = allNames(repos);
+    const languages = await getLanguages(owner, names);
+    const size = getSize(languages);
+    ctx.response.status = 200;
+    ctx.response.body = size;
+  } catch (e) {
+    console.error('error with get', e);
+    ctx.response.status = 200;
+  }
+});
 
 export default router;
