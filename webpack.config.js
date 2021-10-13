f249d29cb03316fec67f45f6c1fc0930c7ffc483
chiefmikey <wolfemikl@gmail.com>
2021-10-13 12:05:01 -0600

    Init webpack

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+import path from 'path';
+
+const SRC_DIR = path.join(path.resolve(), '/client/src');
+const DIST_DIR = path.join(path.resolve(), '/client/public/dist');
+
+export default {
+  mode: 'development',
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  node: 'current',
+                },
+              },
+            ],
+          ],
+        },
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader',
+      },
+      {
+        test: /\.(png|ttf|jp(e*)g|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: '100000&name=img/[name].[ext]' },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.mjs', '.cjs', '.tsx', '.ts'],
+  },
+  devtool: 'inline-source-map',
+};
