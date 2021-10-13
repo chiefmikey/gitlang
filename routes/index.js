32a54205a48fd234579c54789ee9b6d2389af593
chiefmikey <wolfemikl@gmail.com>
2021-10-13 09:12:13 -0600

    Format index routes

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,6 @@ import getRouter from './requests/get.js';
 
 const router = new Router({ prefix: '/' });
 
-router.use(
-  getRouter.routes(),
-  getRouter.allowedMethods(),
-);
+router.use(getRouter.routes(), getRouter.allowedMethods());
 
 export default router;
