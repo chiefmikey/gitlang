65ff314519901fd853aee9ded5f437b8e12c070b
chiefmikey <wolfemikl@gmail.com>
2021-10-13 11:44:12 -0600

    Rename and refactor

---
+++ routes/requests/languages.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const fetchLanguage = async (owner, repo) => {
+  try {
+    const options = {
+      url: `https://api.github.com/repos/${owner}/${repo}/languages`,
+      method: 'get',
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+        Authorization: `token ${process.env.TOKEN}`,
+      },
+    };
+    const res = await axios(options);
+    return res.data;
+  } catch (e) {
+    return e;
+  }
+};
+
+const getLanguages = async (owner, names) => {
+  const languages = [];
+  for (let i = 0; i < names.length; i += 1) {
+    const repo = names[i];
+    languages.push(fetchLanguage(owner, repo));
+  }
+  // eslint-disable-next-line compat/compat
+  return Promise.all(languages);
+};
+
+export default getLanguages;
