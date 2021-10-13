3cc3d16a78424d850ab3da50938c64f8b5179641
chiefmikey <wolfemikl@gmail.com>
2021-10-13 11:45:09 -0600

    Include size helper

---
+++ routes/helpers/size.js
@@ -0,0 +1,39 @@
+const percentage = (size, allSizes) => {
+  const allResults = {};
+  const keys = Object.keys(allSizes);
+  for (let i = 0; i < keys.length; i += 1) {
+    allResults[keys[i]] = (allSizes[keys[i]] / size) * 100;
+  }
+  return allResults;
+};
+
+const eachSize = (languages) => {
+  const allSizes = {};
+  for (let i = 0; i < languages.length; i += 1) {
+    const keys = Object.keys(languages[i]);
+    for (let j = 0; j < keys.length; j += 1) {
+      if (allSizes[keys[j]]) {
+        allSizes[keys[j]] += languages[i][keys[j]];
+      } else {
+        allSizes[keys[j]] = languages[i][keys[j]];
+      }
+    }
+  }
+  return allSizes;
+};
+
+const totalSize = (languages) => {
+  let size = 0;
+  for (let i = 0; i < languages.length; i += 1) {
+    const keys = Object.keys(languages[i]);
+    for (let j = 0; j < keys.length; j += 1) {
+      size += languages[i][keys[j]];
+    }
+  }
+  return size;
+};
+
+const getSize = (languages) =>
+  percentage(totalSize(languages), eachSize(languages));
+
+export default getSize;
