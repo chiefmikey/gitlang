5a0ec55fafb9188522d227efcf3afd93e94deecd
chiefmikey <wolfemikl@gmail.com>
2021-10-13 23:08:16 -0600

    Include decription under title

--- client/src/components/App.svelte
+++ client/src/components/App.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Styles from './Styles.svelte';
   import axios from 'axios';
   import Progress from './Progress.svelte';
 
@@ -30,7 +31,12 @@
 </script>
 
 <template>
+  <Styles />
   <h1>Profile Languages</h1>
+
+  <h6>
+    View language usage from all public repositories of a GitHub user profile
+  </h6>
 
   <input bind:value={owner} placeholder="GitHub Username" />
   <button on:click={submit}>Submit</button>
@@ -52,69 +58,5 @@
   {/if}
 </template>
 
-<style global>
-  body {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    font-family: Arial, Helvetica, sans-serif;
-  }
-
-  #app {
-    width: 40px;
-    height: 60px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-
-  input,
-  button {
-    width: 400px;
-    border: none;
-    box-sizing: border-box;
-    text-align: center;
-    padding: 14px;
-    font-size: 14px;
-    outline: none;
-  }
-
-  button {
-    opacity: 50%;
-  }
-
-  button:hover {
-    opacity: 100%;
-  }
-
-  input {
-    font-size: 18px;
-  }
-
-  span {
-    padding: 0 10px 0 10px;
-  }
-
-  tr {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-  }
-
-  h1 {
-    text-align: center;
-    font-size: 30px;
-    white-space: nowrap;
-    padding: 40px 0 20px 0;
-  }
-
-  h4 {
-    font-size: 16px;
-    white-space: nowrap;
-  }
-
-  #results {
-    padding: 40px 0 40px 0;
-    text-align: center;
-  }
+<style>
 </style>
