29cdd5590b74614c10cd53c846e79cf56ed01cf8
chiefmikey <wolfemikl@gmail.com>
2021-10-14 13:19:10 -0600

    Include results card

---
+++ client/src/components/Card.svelte
@@ -0,0 +1,60 @@
+<script>
+  export let langCount;
+  export let repoCount;
+  export let currentOwner;
+  export let data;
+
+  const url = `https://github.com/${currentOwner}`;
+</script>
+
+<template>
+  <div id="card">
+    {#if currentOwner}
+      <a id="owner" href={url}>{currentOwner}</a>
+    {/if}
+    {#if data && data.length > 0}
+      {#if repoCount || langCount}
+        <div id="counts">
+          {#if repoCount}
+            <span id="repos">Repos: {repoCount}</span>
+          {/if}
+          {#if langCount}
+            <span id="lang">Languages: {langCount}</span>
+          {/if}
+        </div>
+      {/if}
+    {:else}
+      <h6 id="loading">Loading...</h6>
+    {/if}
+  </div>
+</template>
+
+<style>
+  #card {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #owner {
+    font-size: 20px;
+    padding: 10px 10px 10px 10px;
+    margin-bottom: 12px;
+    animation: fadeIn 0.1s ease 0s forwards;
+    border-bottom: 1px solid black;
+  }
+  #counts {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding-bottom: 15px;
+  }
+
+  #repos,
+  #lang {
+    font-size: 15px;
+    animation: fadeIn 0.1s ease 0s forwards;
+  }
+</style>
