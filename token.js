18bedb9f1230d52bb88e06e989dffe79c7156865
chiefmikey <wolfemikl@gmail.com>
2021-10-20 13:48:02 -0600

    Update to aws-sdk v3

--- token.js
+++ token.js
@@ -1,27 +1,23 @@
-import AWS from 'aws-sdk';
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from '@aws-sdk/client-secrets-manager';
 
 const region = 'us-east-2';
 const secretName = 'profile-languages';
 let secret;
 
-const client = new AWS.SecretsManager({
+const client = new SecretsManagerClient({
   region,
 });
 
-client.getSecretValue({ SecretId: secretName }, (err, data) => {
-  if (
-    err &&
-    (err.code === 'DecryptionFailureException' ||
-      err.code === 'InternalServiceErrorException' ||
-      err.code === 'InvalidParameterException' ||
-      err.code === 'InvalidRequestException' ||
-      err.code === 'ResourceNotFoundException')
-  ) {
-    throw err;
-  } else if (data && data.SecretString) {
-    secret = data.SecretString;
-  }
-});
+const data = await client.send(
+  new GetSecretValueCommand({ SecretId: secretName }),
+);
+
+if (data && data.SecretString) {
+  secret = data.SecretString;
+}
 
 const token = () => JSON.parse(secret).TOKEN;
 
