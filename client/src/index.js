4ae352f59fe94a2b0fd2e1277782980b7a90a75e
chiefmikey <wolfemikl@gmail.com>
2021-10-23 11:38:34 -0600

    Init smoothscroll polyfill

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,7 @@
+import smoothscroll from '../../node_modules/smoothscroll-polyfill/dist/smoothscroll.js';
 import App from './components/App.svelte';
+
+smoothscroll.polyfill();
 
 const app = new App({
   target: document.getElementById('app'),
