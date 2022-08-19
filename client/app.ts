cf66564d25b43ea877bf3d88b0ebc7391d11dc9e
chiefmikey <wolfemikl@gmail.com>
2022-08-19 11:43:56 -0600

    Update local server config

--- client/app.ts
+++ client/app.ts
@@ -1,14 +1,35 @@
-import path from 'node:path';
+import { createReadStream } from 'node:fs';
 
 import Koa from 'koa';
 import serve from 'koa-static';
 
-const port = 3004;
-
 const app = new Koa();
+const port = 3000;
+const redirect = 'docs';
+const subdir = 'public';
 
-app.use(serve(path.join(path.resolve(), '/docs')));
-
-app.listen(port, () => console.log('Koa is listening on port', port));
+app
+  .use(serve(redirect))
+  .use(async (context, next) => {
+    try {
+      if (context.status === 404) {
+        const url = context.url.split(subdir)[1];
+        if (url) {
+          const path = `${redirect}/${subdir}${url}`;
+          context.type = 'html';
+          context.body = createReadStream(path);
+        } else {
+          context.type = 'html';
+          context.body = createReadStream(`${redirect}/404.html`);
+        }
+      } else {
+        await next();
+      }
+    } catch {
+      context.type = 'html';
+      context.body = createReadStream(`${redirect}/404.html`);
+    }
+  })
+  .listen(port, () => console.log(`Koa is listening port: ${port}`));
 
 export default app;
