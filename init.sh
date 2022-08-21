260d089e7ee5989f31ede69e06badc0a1ad1facb
chiefmikey <wolfemikl@gmail.com>
2022-08-21 14:29:08 -0600

    Update server dir

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ -f "/home/ec2-user/auth/server/init.sh" ]; then
-  chmod +x /home/ec2-user/auth/server/init.sh
-  /home/ec2-user/auth/server/init.sh
+if [ -f "/home/ec2-user/server/init.sh" ]; then
+  chmod +x /home/ec2-user/server/init.sh
+  /home/ec2-user/server/init.sh
 # else run auth-server deploy workflow
 fi
--- stop.sh
+++ stop.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ -f "/home/ec2-user/auth/server/stop.sh" ]; then
-  chmod +x /home/ec2-user/auth/server/stop.sh
-  /home/ec2-user/auth/server/stop.sh
+if [ -f "/home/ec2-user/server/stop.sh" ]; then
+  chmod +x /home/ec2-user/server/stop.sh
+  /home/ec2-user/server/stop.sh
 # else run auth-server deploy workflow
 fi
