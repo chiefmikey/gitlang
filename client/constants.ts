9591cbcb524f738ec183f9ddee95bfd8b5100cd2
chiefmikey <wolfemikl@gmail.com>
2022-08-27 23:37:30 -0600

    Add constants

---
+++ client/constants.ts
@@ -0,0 +1,11 @@
+export const ROUTES = {
+  REPOS: 'https://api.5105015032.com/gitlang/github/repos',
+  LANGS: 'https://api.5105015032.com/gitlang/github/langs',
+  REPOS_LOCAL: ':3000/gitlang/github/repos',
+  LANGS_LOCAL: ':3000/gitlang/github/langs',
+};
+
+export const BETA = {
+  REPOS: 'https://api.5105015032.com/gitlang-beta/github/repos',
+  LANGS: 'https://api.5105015032.com/gitlang-beta/github/langs',
+};
