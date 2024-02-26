81053dc101e4327cd28ba9835f1af9576caf51d2
Mikl Wolfe <wolfemikl@gmail.com>
2024-02-26 08:49:56 -0700

    feat(client): migrate deployment to s3

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -f "/home/ec2-user/server/init.sh" ]; then
-  chmod +x /home/ec2-user/server/init.sh
-  /home/ec2-user/server/init.sh
-# else run auth-server deploy workflow
-fi
--- stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -f "/home/ec2-user/server/stop.sh" ]; then
-  chmod +x /home/ec2-user/server/stop.sh
-  /home/ec2-user/server/stop.sh
-# else run auth-server deploy workflow
-fi
