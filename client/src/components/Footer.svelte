a7450b3208a03948f187820535c49f2aed6b16a6
Mikl Wolfe <wolfemikl@gmail.com>
2024-03-17 15:45:16 -0600

    feat(client): refactor footer to component

---
+++ client/src/components/Footer.svelte
@@ -0,0 +1,17 @@
+<template>
+  <h6 class="footer">
+    <a
+      href="https://github.com/chiefmikey"
+      rel="noopener noreferrer"
+      target="_blank"
+      >made by Chief Mikey</a
+    >
+  </h6>
+</template>
+
+<style>
+  .footer {
+    padding-bottom: 2rem;
+    color: #fe9fc9;
+  }
+</style>
