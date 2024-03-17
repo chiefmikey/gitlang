e0fce87f71dbbbc60de7d62fbc373aaf9fb08bf1
Mikl Wolfe <wolfemikl@gmail.com>
2024-03-17 15:45:27 -0600

    feat(client): refactor results to component

---
+++ client/src/components/Results.svelte
@@ -0,0 +1,58 @@
+<script>
+  import { ERROR } from '../../constants';
+
+  import Card from './Card.svelte';
+  import Progress from './Progress.svelte';
+
+  export let current;
+  export let data;
+  export let count1;
+  export let count2;
+  export let isDone;
+</script>
+
+<template>
+  <div class="results">
+    {#if current}
+      <Card
+        {count1}
+        {count2}
+        {current}
+        {data}
+      />
+    {/if}
+    {#if data}
+      <table>
+        <tbody>
+          {#if data.length > 0}
+            {#each data as dat, index (index)}
+              <Progress
+                {count1}
+                {dat}
+                {index}
+                {isDone}
+              />
+            {/each}
+          {:else}
+            <h4 class="error">{ERROR.NOT_FOUND}</h4>
+          {/if}
+        </tbody>
+      </table>
+    {/if}
+  </div>
+</template>
+
+<style>
+  .results {
+    width: 100%;
+    padding: 0 0 10px 0;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .error {
+    padding: 2rem;
+  }
+</style>
