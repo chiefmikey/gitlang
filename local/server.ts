0897aea825db9c43c62da6757efefc0265ac9a8e
Mikl Wolfe <wolfemikl@gmail.com>
2024-04-09 11:00:37 -0600

    feat(local): update server

--- local/server.ts
+++ local/server.ts
@@ -1,4 +1,3 @@
-import cors from '@koa/cors';
 import { config } from 'dotenv';
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
@@ -11,13 +10,7 @@ const app = new Koa();
 
 const port = 3000;
 
-const corsOptions = {
-  allowMethods: ['GET', 'POST'],
-  maxAge: 600,
-};
-
 app
-  .use(cors(corsOptions))
   .use(bodyParser())
   .use(gitlang.routes())
   .use(gitlang.allowedMethods())
